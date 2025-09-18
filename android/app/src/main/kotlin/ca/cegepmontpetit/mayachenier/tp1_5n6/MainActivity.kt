a1ecf3655c10cd4ace2ec850c53bfbe34ff27f2b
Maya <145165822+MayaChen350@users.noreply.github.com>
2025-09-18 18:36:19 -0400

    INIT

---
+++ android/app/src/main/kotlin/ca/cegepmontpetit/mayachenier/tp1_5n6/MainActivity.kt
@@ -0,0 +1,5 @@
+package ca.cegepmontpetit.mayachenier.tp1_5n6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/ui/screens/test_other_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+
+class OtherScreen extends StatefulWidget {
+  const OtherScreen({super.key});
+
+  @override
+  State<OtherScreen> createState() => _OtherScreenState();
+}
+
+class _OtherScreenState extends State<OtherScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder(
+    );
+  }
+}
